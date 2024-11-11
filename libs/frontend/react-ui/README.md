81f4ae7e7b47293bdc26959cc388e94646954e52
mobassherkhandakar <mobassherkhandakar@gmail.com>
2024-11-12 03:56:10 +0600

    libs folder created

---
+++ libs/frontend/react-ui/README.md
@@ -0,0 +1,7 @@
+# frontend-react-ui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test frontend-react-ui` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/frontend/react-ui/src/lib/frontend-react-ui.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledFrontendReactUi = styled.div`
+  color: pink;
+`;
+
+export function FrontendReactUi() {
+  return (
+    <StyledFrontendReactUi>
+      <h1>Welcome to FrontendReactUi!</h1>
+    </StyledFrontendReactUi>
+  );
+}
+
+export default FrontendReactUi;
